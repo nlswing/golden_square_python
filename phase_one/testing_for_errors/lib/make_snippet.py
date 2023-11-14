17c748bf51dcef66720eb43d2d999f2c80ce949d
Nathan Wing <wing_nathan@hotmail.com>
2023-11-14 10:46:43 +0000

    02 Exercise completed - all tests passing.

--- phase_one/testing_for_errors/lib/make_snippet.py
+++ phase_one/testing_for_errors/lib/make_snippet.py
@@ -1,2 +1,13 @@
-def make_snippet(str):
-    return str
+def make_snippet(words):
+    if len(words.split()) <= 5:
+        return words
+    else:
+        split_words = words.split()
+        first_five_words = split_words[0:5]
+        create_string = ' '.join(first_five_words)
+        return create_string + '...'
+        
+   
+        
+    
+    
--- phase_one/testing_for_errors/tests/test_make_snippet.py
+++ phase_one/testing_for_errors/tests/test_make_snippet.py
@@ -7,3 +7,24 @@ Function returns a string that is passed as an argument.
 def test_returns_string_when_passed_as_argument():
     result = make_snippet('test')
     assert result == 'test'
+
+"""
+Words returned as when five words.
+"""
+    
+def test_return_words_when_five_words():
+    result = make_snippet('There are five words here')
+    assert result == 'There are five words here'
+    
+"""
+Words and '...' returned if more than five words in string
+"""
+
+def test_returns_words_and_dots_if_more_than_five():
+    result = make_snippet('There are more than five words here')
+    assert result == 'There are more than five...'
+
+    
+
+    
+    
