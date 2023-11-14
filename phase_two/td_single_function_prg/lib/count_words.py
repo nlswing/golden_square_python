c63de3bb1b11ee28a65b754b3efb92efb6eddfdf
Nathan Wing <wing_nathan@hotmail.com>
2023-11-14 11:19:00 +0000

    02 Challenge completed.

--- phase_two/td_single_function_prg/lib/count_words.py
+++ phase_two/td_single_function_prg/lib/count_words.py
@@ -1,9 +1,2 @@
-from lib.count_words import *
-
-"""
-Returns 0 for an empty string.
-"""
-
-def test_returns_zero_for_empty_string():
-    result = count_words("")
-    assert result == 0
+def count_words(string):
+    return len(string.split())
--- phase_two/td_single_function_prg/tests/test_count_words.py
+++ phase_two/td_single_function_prg/tests/test_count_words.py
@@ -1,2 +1,33 @@
-def count_words(string):
-    pass
+from lib.count_words import *
+
+"""
+Returns 0 for an empty string.
+"""
+
+def test_returns_zero_for_empty_string():
+    result = count_words("")
+    assert result == 0
+    
+"""
+Returns 1 for a string that contains one word.
+"""
+
+def test_returns_one_for_one_word_string():
+    result = count_words('word')
+    assert result == 1
+    
+"""
+Returns 5 for a string that contains 5 words.
+"""
+
+def test_returns_five_for_string_of_five_words():
+    result = count_words('This is five words long.')
+    assert result == 5
+
+"""
+Returns 100 for a 100 word string.
+"""   
+
+def test_returns_one_hundered_for_one_hundred_word_string():
+    result = count_words('hello ' * 100)
+    assert result == 100
